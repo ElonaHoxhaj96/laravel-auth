4e0d9db40ebbe9cff269848d020a9e6b86c82b55
ElonaHoxhaj96 <elona6915@gmail.com>
2024-09-20 16:42:45 +0200

    Feat index.blade

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -17,7 +17,7 @@
                     </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <p>Welcome to your personalized page!</p>
                 </div>
             </div>
         </div>
