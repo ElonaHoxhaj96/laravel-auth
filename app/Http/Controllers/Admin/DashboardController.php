1d3871985d77454b5ce119debece05600c28beb8
ElonaHoxhaj96 <elona6915@gmail.com>
2024-09-24 17:25:20 +0200

    feat:counting of posts

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {   
     public function index(){
-       return view('admin.index');
+        $count_post = Post::count();
+
+       return view('admin.index', compact('count_post'));
     }
 }
